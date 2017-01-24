7dc2095b41d557e38cfbfb60cdd714b582d19527
K Lauer <klauer@slac.stanford.edu>
2017-01-24 14:04:42 -0800

    Keep track of Ethercat bus cycles for each Linmot controller

--- motorApp/linmotSrc/LinmotMotorDriver.h
+++ motorApp/linmotSrc/LinmotMotorDriver.h
@@ -1,15 +1,17 @@
+// vi: sw=2 ts=2 sts=2 expandtab
 /*
 FILENAME...   LinmotMotorDriver.h
 USAGE...      Motor driver support for the Parker Linmot series of controllers, including the Aries.
 
 Mark Rivers
 March 28, 2011
-
 */
+
 
 #include "asynMotorController.h"
 #include "asynMotorAxis.h"
 #include <asynPortClient.h>
+#include <vector>
 
 class epicsShareClass LinmotAxis : public asynMotorAxis
 {
@@ -23,22 +25,22 @@ public:
   virtual asynStatus poll(bool *moving);
   virtual asynStatus setClosedLoop(bool closedLoop);
 
-  
+
   asynStatus sendCmd( int command, int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0, int param5 = 0);
 
 private:
   class LinmotController *pC_;      /**< Pointer to the asynMotorController to which this axis belongs.
                                 *   Abbreviated because it is used very frequently */
-  char axisName_[10];      /**< Name of each axis, used in commands to Linmot controller */ 
-  double pulsesPerUnit_;   /**< Pulses per engineering unit, which is what Linmot controller uses */ 
-  int flagsReg_;           /**< Address of the flags register */ 
-  int limitsReg_;          /**< Address of the limits register */ 
-  int encoderPositionReg_; /**< Address of the encoder position register */ 
-  int theoryPositionReg_;  /**< Address of the theoretical position register */ 
-  double encoderPosition_; /**< Cached copy of the encoder position */ 
-  double theoryPosition_;  /**< Cached copy of the theoretical position */ 
-  int currentFlags_;       /**< Cached copy of the current flags */ 
-  int currentLimits_;      /**< Cached copy of the current limits */ 
+  char axisName_[10];      /**< Name of each axis, used in commands to Linmot controller */
+  double pulsesPerUnit_;   /**< Pulses per engineering unit, which is what Linmot controller uses */
+  int flagsReg_;           /**< Address of the flags register */
+  int limitsReg_;          /**< Address of the limits register */
+  int encoderPositionReg_; /**< Address of the encoder position register */
+  int theoryPositionReg_;  /**< Address of the theoretical position register */
+  double encoderPosition_; /**< Cached copy of the encoder position */
+  double theoryPosition_;  /**< Cached copy of the theoretical position */
+  int currentFlags_;       /**< Cached copy of the current flags */
+  int currentLimits_;      /**< Cached copy of the current limits */
   int done_;
   int dInput1_;
   int dInput2_;
@@ -57,7 +59,6 @@ private:
   int demandCurrent_;
 
   int digitalInputsWord_;
-
 friend class LinmotController;
 };
 
@@ -70,9 +71,11 @@ public:
   LinmotAxis* getAxis(asynUser *pasynUser);
   LinmotAxis* getAxis(int axisNo);
 
-  
+
   /* These are the methods that are new to this class */
   asynStatus readBinaryIO();
+  void cycleThreadLoop();  // This should be private but is called from C function
+
 
 protected:
   asynInt32Client *stateVar_;
@@ -81,7 +84,7 @@ protected:
   asynInt32Client *demandPosition_;
   asynInt32Client *actualPosition_;
   asynInt32Client *demandCurrent_;
- 
+
   asynInt32Client *controlWord_;
   asynInt32Client *commandHeader_;
   asynInt32Client *commandParam1_;
@@ -91,5 +94,15 @@ protected:
   asynInt32Client *commandParam5_;
 
   asynInt32Client *digitalInputsWord_;
+  asynInt32Client *ethercatCycle_;
+
+  epicsEvent cycleEvent_;
+  static bool cycleCallbackRegistered_;
+  static void sCycleCallback(void *, asynUser *, epicsInt32 data);
+  static std::vector<epicsEvent*> cycleEventVector_;
+
 friend class LinmotAxis;
 };
+
+
+static void CycleThreadC(void *drvPvt);
